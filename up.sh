f6e63186ffda8ae20a25e82d5e8f3c715aac2164
Yuriy Kirillov <ykirillov@icloud.com>
2024-12-16 16:54:15 +0100

    add DOMAIN env

--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker compose -f ./traefik/docker-compose.yml up -d
-docker compose -f ./portainer/docker-compose.yml up -d
-docker compose -f ./whoami/docker-compose.yml up -d
+docker compose --env-file .env -f ./traefik/docker-compose.yml up -d
+docker compose --env-file .env -f ./portainer/docker-compose.yml up -d
+docker compose --env-file .env -f ./whoami/docker-compose.yml up -d

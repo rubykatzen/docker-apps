24c0c97b68b702f2cd4ee4f306b500590cee4c7a
Yuriy Kirillov <ykirillov@icloud.com>
2024-12-16 17:02:26 +0100

    add beszel

--- down.sh
+++ down.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker compose -f ./traefik/docker-compose.yml down
-docker compose -f ./portainer/docker-compose.yml down
-docker compose -f ./whoami/docker-compose.yml down
+docker compose --env-file .env -f ./traefik/docker-compose.yml down
+docker compose --env-file .env -f ./portainer/docker-compose.yml down
+docker compose --env-file .env -f ./whoami/docker-compose.yml down
+docker compose --env-file .env -f ./beszel/docker-compose.yml down
--- up.sh
+++ up.sh
@@ -3,3 +3,4 @@
 docker compose --env-file .env -f ./traefik/docker-compose.yml up -d
 docker compose --env-file .env -f ./portainer/docker-compose.yml up -d
 docker compose --env-file .env -f ./whoami/docker-compose.yml up -d
+docker compose --env-file .env -f ./beszel/docker-compose.yml up -d

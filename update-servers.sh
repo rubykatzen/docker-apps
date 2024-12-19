5667b96f842dee416d34eaa975f0863de66a195e
Yuriy Kirillov <ykirillov@icloud.com>
2024-12-20 00:47:33 +0100

    update-servers.sh update

--- update-servers.sh
+++ update-servers.sh
@@ -2,6 +2,10 @@
 source servers.env
 for server in "${SERVERS[@]}"
 do
+  echo "============================================="
+  echo "update ${server}"
+  echo "-->"
   ssh -A "${server}" 'cd docker-apps; git pull'
   ssh -A "${server}" 'cd docker-apps; ./update.sh'
+  ssh -A "${server}" 'cd docker-apps; docker image prune'
 done

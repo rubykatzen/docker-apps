c50661f656d6ed2e05910131a5931daf034b192f
Yuriy Kirillov <ykirillov@icloud.com>
2025-01-13 17:40:07 +0100

    drop APPS[@]

--- update.sh
+++ update.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 source apps.env
-for app in "${APPS[@]}"
-do
-  docker compose --env-file ./"${app}"/.env --env-file .env -f ./"${app}"/docker-compose.yml pull
-  docker compose --env-file ./"${app}"/.env --env-file .env -f ./"${app}"/docker-compose.yml up -d
-done
-
 for app in "${DAPPS[@]}"
 do
   docker compose --env-file ./apps/"${app}"/.env --env-file .env -f ./apps/"${app}"/docker-compose.yml pull

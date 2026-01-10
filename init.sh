76b6b41fca4936ad75b45d2379de09051d39cb69
Yuriy Kirillov <ykirillov@icloud.com>
2026-01-10 20:37:27 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cp .env.example .env
 cp apps.env.example apps.env
+
 mkdir apps-data
 mkdir apps-data/traefik
 touch apps-data/traefik/acme.json
 chmod 600 apps-data/traefik/acme.json
+
 docker network create traefik
 docker network create databases
